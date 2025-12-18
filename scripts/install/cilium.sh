3d8a1e6d005ce541979ff204a1a5a938546f67ce
Ethan Lee <ethantlee21@gmail.com>
2025-12-18 13:58:34 -0500

    Chore: Refactor install scripts into taskfile
    - Removes individual install scripts for argocd, cilium, tailscale, and cnpg secrets.
    - Moves the installation logic directly into the taskfile for better task management.
    - Modifies taskfile to use helm commands directly for package management and deployment.
    - Includes a variable for password generation for cnpg secrets.

--- scripts/install/cilium.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-helm repo add cilium https://helm.cilium.io/
-helm repo update
-
-helm upgrade --install cilium cilium/cilium \
-  --namespace kube-system \
-  -f bootstrap/cilium/values.yaml \
-  --wait
